37f3695486fe0b1b08347285b80dd34e324a715f
eli7eb <eli7eb@gmail.com>
2021-08-07 20:56:52 +0300

    center buttons of main screen. added back button to the rest of screens

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ kivy.require("1.10.1")  # make sure people running py file have right version
 
 # Our simple app. NameApp  convention matters here. Kivy
 # uses some magic here, so make sure you leave the App bit in there!
-
+# pos_hint = {“x”:1, “y”:1, “left”:1, “right”:1, “center_x”:1,
+#                  “center_y”:1, “top”:1, “bottom”:1(“top”:0)}
 
 class MainScreen(Screen):
     pass
@@ -36,33 +37,3 @@ class MainApp(App):
 # Run the app.
 if __name__ == "__main__":
     MainApp().run()
-
-from src.game_consts.game_constants import LEVEL_BEGIN
-
-from pygame.locals import *
-from src.game_views.load_resource_view import LoadingView
-from src.game_views.text_view import TextView
-from src.game_views.main_menu_view import MenuView
-from src.game_views.quit_view import QuitView
-from src.game_views.game_view import GameView
-from src.game_consts.game_constants import *
-from src.game_utils.game_logger import RkLogger
-from src.game_consts.game_constants import *
-
-# TODO make the players think before trying so reward on success at first attempt
-# TODO and minus score on successive same tries
-# TODO non equal tiles for next version
-
-
-
-WIDTH, HEIGHT = 750, 750
-level = levels[1]
-# load resources
-# constants
-WIDTH, HEIGHT = 750, 750
-
-
-HALF_SCREEN_WIDTH = SCREEN_WIDTH / 2
-HALF_SCREEN_HEIGHT = SCREEN_HEIGHT / 2
-
-
